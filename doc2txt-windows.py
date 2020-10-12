70676a38e8889c8fb235dd2c74b7129c4f329b64
CuriousLiu <004liuyixuan@gmail.com>
2020-10-12 15:52:11 +0800

    doc2txt-windows.py 驼峰命名法代码优化
    doc2txt-windows.py 驼峰命名法代码优化

--- doc2txt-windows.py
+++ doc2txt-windows.py
@@ -3,39 +3,35 @@ import sys
 import fnmatch
 import win32com.client
 
-#PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
-doc_path = 'C:\\Users\\Administrator\\Desktop\\a\\ALL\\'
-txt_path = 'C:\\Users\\Administrator\\Desktop\\b\\'
+docPath = ''
+txtPath = ''
 
-def convert_dir_to_txt():
-    """
-    将默认整个文件夹下的文件都进行转换
-    :return:
-    """
+def convertDocToTxt():
     i = 0
-    for root, dirs, files in os.walk(doc_path):
+    for root, dirs, files in os.walk(docPath):
         for _dir in dirs:
             pass
         for _file in files:
             if fnmatch.fnmatch(_file, '*.doc'):
-                store_file = txt_path + _file[:-3] + 'txt'
+                storeFile = txtPath + _file[:-3] + 'txt'
             elif fnmatch.fnmatch(_file, '*.docx'):
-                store_file = txt_path + _file[:-4] + 'txt'
-            word_file = os.path.join(root, _file)
-            dealer.Documents.Open(word_file)
+                storeFile = txtPath + _file[:-4] + 'txt'
+                
+            wordFile = os.path.join(root, _file)
+            dealer.Documents.Open(wordFile)
             try:
-                dealer.ActiveDocument.SaveAs(store_file, FileFormat=7,Encoding=65001)
+                dealer.ActiveDocument.SaveAs(storeFile, FileFormat=7,Encoding=65001)
             except Exception as e:
                 print(e)
             dealer.ActiveDocument.Close()
-            #print()
-            os.remove(doc_path+_file)
+
+            os.remove(docPath+_file)
+            
             print("正在制作第",i)
             i += 1
  
 dealer = win32com.client.gencache.EnsureDispatch('Word.Application')
 
-
 if __name__== '__main__':
 
-    convert_dir_to_txt()
+    convertDocToTxt()
